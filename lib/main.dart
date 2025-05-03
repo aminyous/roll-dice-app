55817794e3505d16cc2c13eea02b915cd9bc0b6d
Amine Hichri <amyn.hichri@gmail.com>
2025-05-03 14:06:14 +0200

    My First Commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // This is the entry point of the application.
+  // It calls the runApp function to start the app.
+  // The MyApp widget is passed to runApp to build the UI.
+  // The MyApp widget is defined in the my_app.dart file.
+  runApp(const MaterialApp(
+    home: Scaffold(
+      body: Center(
+        child: Text("First App By Me (Amine)"),
+      ),
+    ),
+  ));
+} 
