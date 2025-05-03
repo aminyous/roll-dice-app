a5f3ea44525ea4db5c50e243b9beecc5a3813085
Amine Hichri <amyn.hichri@gmail.com>
2025-05-03 15:38:18 +0200

    Add gradient

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,31 @@ void main() {
   // It calls the runApp function to start the app.
   // The MyApp widget is passed to runApp to build the UI.
   // The MyApp widget is defined in the my_app.dart file.
-  runApp(const MaterialApp(
-    home: Scaffold(
-      body: Center(
-        child: Text("First App By Me (Amine)"),
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 26, 2, 80),
+                Color.fromARGB(255, 45, 7, 98),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              "First App By Me (Amine)",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+              ),
+            ),
+          ),
+        ),
       ),
     ),
-  ));
-} 
+  );
+}
